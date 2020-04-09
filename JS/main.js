5d3b12b8589f383b6e24a79f7124493910272dc6
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-09 18:08:04 +0200

    JS
    Fine indice

--- JS/main.js
+++ JS/main.js
@@ -14,23 +14,27 @@ jQuery(document).ready(function ($) {
             $('.first-index').removeClass('index-image-color');
             $('.first-index').next().addClass('index-image-color');
             
-        
-        } else if ( ( $('.first').next().hasClass('active') ) && ( $('.first-index').next().hasClass('index-image-color'))) {
+         
+        } else if ( $('.first').next().hasClass('active') ) {
             
             $('.first').next().removeClass('active');           
             $('.last').prev().addClass('active');   
             $('.first-index').next().removeClass('index-image-color');
-            $('.last-index').perv().addClass('index-image-color');
+            $('.last-index').prev().addClass('index-image-color');
 
-        } else if ( $('.last').prev().hasClass('active')) {
+        } else if ( $('.last').prev().hasClass('active') ) {
 
             $('.last').prev().removeClass('active');
             $('.last').addClass('active');
+            $('.last-index').prev().removeClass('index-image-color');
+            $('.last-index').addClass('index-image-color');
             
         } else {
 
             $('.last').removeClass('active');
             $('.first').addClass('active');
+            $('.last-index').removeClass('index-image-color');
+            $('.first-index').addClass('index-image-color');
         
         }
 
@@ -42,23 +46,31 @@ jQuery(document).ready(function ($) {
             
             $('.first').removeClass('active').addClass('display_none');
             $('.last').addClass('active');
+            $('.first-index').removeClass('index-image-color');
+            $('.last-index').addClass('index-image-color');
 
         
         } else if ( $('.last').hasClass('active') ) {
             
             $('.last').removeClass('active');           
             $('.last').prev().addClass('active');
+            $('.last-index').prev().addClass('index-image-color');
+            $('.last-index').removeClass('index-image-color');
 
         } else if ( $('.last').prev().hasClass('active')) {
 
             $('.last').prev().removeClass('active');
             $('.first').next().addClass('active');
+            $('.first-index').next().addClass('index-image-color');
+            $('.last-index').prev().removeClass('index-image-color');
             
         } else {
 
             $('.first').next().removeClass('active');
             $('.first').addClass('active');
-        
+            $('.first-index').addClass('index-image-color');
+            $('.first-index').next().removeClass('index-image-color');
+            
         }
 
     });
@@ -70,26 +82,34 @@ jQuery(document).ready(function ($) {
 
         if (e.keyCode == 39) {
             
-            if ( $('.first').hasClass('active') ) {
+            if (( $('.first').hasClass('active') ) && ( $('.first-index').hasClass('index-image-color') ) ) {
             
                 $('.first').removeClass('active').addClass('display_none');
                 $('.first').next().addClass('active');
-    
-            
+                $('.first-index').removeClass('index-image-color');
+                $('.first-index').next().addClass('index-image-color');
+                
+             
             } else if ( $('.first').next().hasClass('active') ) {
                 
                 $('.first').next().removeClass('active');           
-                $('.last').prev().addClass('active');
+                $('.last').prev().addClass('active');   
+                $('.first-index').next().removeClass('index-image-color');
+                $('.last-index').prev().addClass('index-image-color');
     
-            } else if ( $('.last').prev().hasClass('active')) {
+            } else if ( $('.last').prev().hasClass('active') ) {
     
                 $('.last').prev().removeClass('active');
                 $('.last').addClass('active');
+                $('.last-index').prev().removeClass('index-image-color');
+                $('.last-index').addClass('index-image-color');
                 
             } else {
     
                 $('.last').removeClass('active');
                 $('.first').addClass('active');
+                $('.last-index').removeClass('index-image-color');
+                $('.first-index').addClass('index-image-color');
             
             }
 
@@ -99,23 +119,31 @@ jQuery(document).ready(function ($) {
             
                 $('.first').removeClass('active').addClass('display_none');
                 $('.last').addClass('active');
+                $('.first-index').removeClass('index-image-color');
+                $('.last-index').addClass('index-image-color');
     
             
             } else if ( $('.last').hasClass('active') ) {
                 
                 $('.last').removeClass('active');           
                 $('.last').prev().addClass('active');
+                $('.last-index').prev().addClass('index-image-color');
+                $('.last-index').removeClass('index-image-color');
     
             } else if ( $('.last').prev().hasClass('active')) {
     
                 $('.last').prev().removeClass('active');
                 $('.first').next().addClass('active');
+                $('.first-index').next().addClass('index-image-color');
+                $('.last-index').prev().removeClass('index-image-color');
                 
             } else {
     
                 $('.first').next().removeClass('active');
                 $('.first').addClass('active');
-            
+                $('.first-index').addClass('index-image-color');
+                $('.first-index').next().removeClass('index-image-color');
+                
             }
             
         }
