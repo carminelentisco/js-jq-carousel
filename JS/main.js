65458b44d3be870dbdb9b69855e70a0a9693c334
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-09 17:22:27 +0200

    JS
    Movimento con le frecce aggiunto

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,6 @@ jQuery(document).ready(function ($) {
 
     arrowRight.click(function () { 
         
-
         if ( $('.first').hasClass('active') ) {
             
             $('.first').removeClass('active').addClass('display_none');
@@ -60,4 +59,63 @@ jQuery(document).ready(function ($) {
 
     });
 
+
+    
+    $(document).keydown(function (e) { 
+        console.log(e.keyCode);
+
+        if (e.keyCode == 39) {
+            
+            if ( $('.first').hasClass('active') ) {
+            
+                $('.first').removeClass('active').addClass('display_none');
+                $('.first').next().addClass('active');
+    
+            
+            } else if ( $('.first').next().hasClass('active') ) {
+                
+                $('.first').next().removeClass('active');           
+                $('.last').prev().addClass('active');
+    
+            } else if ( $('.last').prev().hasClass('active')) {
+    
+                $('.last').prev().removeClass('active');
+                $('.last').addClass('active');
+                
+            } else {
+    
+                $('.last').removeClass('active');
+                $('.first').addClass('active');
+            
+            }
+
+        } else if ( e.keyCode == 37 ) {
+            
+            if ( $('.first').hasClass('active') ) {
+            
+                $('.first').removeClass('active').addClass('display_none');
+                $('.last').addClass('active');
+    
+            
+            } else if ( $('.last').hasClass('active') ) {
+                
+                $('.last').removeClass('active');           
+                $('.last').prev().addClass('active');
+    
+            } else if ( $('.last').prev().hasClass('active')) {
+    
+                $('.last').prev().removeClass('active');
+                $('.first').next().addClass('active');
+                
+            } else {
+    
+                $('.first').next().removeClass('active');
+                $('.first').addClass('active');
+            
+            }
+            
+        }
+        
+    });
+    
 }); // Fine Documento 
