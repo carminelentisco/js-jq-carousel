8833aec5de3ace090e413d0d0ef31c9a1f1d7f7d
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-14 11:00:20 +0200

    JS
    Correzioni

--- JS/main.js
+++ JS/main.js
@@ -4,17 +4,117 @@ jQuery(document).ready(function ($) {
     var arrowLeft = $('#arrow-left');
     var arrowRight = $('#arrow-right');
 
-
+    // Scorri a destra
     arrowRight.click(function () { 
+
+        directionImg('arrowRight');
+
+    });
+
+    // Scorri a sinistra
+    arrowLeft.click( function() {
+
+        directionImg('arrowLeft');
         
+    });
+
+    // controllo con i tasti
+    $(document).keydown(function (e) { 
+        console.log(e.keyCode);
+        
+        keyDirection(e.keyCode);
+
+    });
+    
+}); // Fine Documento 
+
+function directionImg(direction) {
+    
+    if (direction === 'arrowRight') {
+    
         if (( $('.first').hasClass('active') ) && ( $('.first-index').hasClass('index-image-color') ) ) {
-            
+                
             $('.first').removeClass('active').addClass('display_none');
             $('.first').next().addClass('active');
             $('.first-index').removeClass('index-image-color');
             $('.first-index').next().addClass('index-image-color');
             
-         
+        
+        } else if ( $('.first').next().hasClass('active') ) {
+            
+            $('.first').next().removeClass('active');           
+            $('.last').prev().addClass('active');   
+            $('.first-index').next().removeClass('index-image-color');
+            $('.last-index').prev().addClass('index-image-color');
+    
+        } else if ( $('.last').prev().hasClass('active') ) {
+    
+            $('.last').prev().removeClass('active');
+            $('.last').addClass('active');
+            $('.last-index').prev().removeClass('index-image-color');
+            $('.last-index').addClass('index-image-color');
+            
+        } else {
+    
+            $('.last').removeClass('active');
+            $('.first').addClass('active');
+            $('.last-index').removeClass('index-image-color');
+            $('.first-index').addClass('index-image-color');
+        
+        }
+        
+    } else if ( direction === 'arrowLeft') {
+
+        if ( $('.first').hasClass('active') ) {
+                
+            $('.first').removeClass('active').addClass('display_none');
+            $('.last').addClass('active');
+            $('.first-index').removeClass('index-image-color');
+            $('.last-index').addClass('index-image-color');
+    
+        
+        } else if ( $('.last').hasClass('active') ) {
+            
+            $('.last').removeClass('active');           
+            $('.last').prev().addClass('active');
+            $('.last-index').prev().addClass('index-image-color');
+            $('.last-index').removeClass('index-image-color');
+    
+        } else if ( $('.last').prev().hasClass('active')) {
+    
+            $('.last').prev().removeClass('active');
+            $('.first').next().addClass('active');
+            $('.first-index').next().addClass('index-image-color');
+            $('.last-index').prev().removeClass('index-image-color');
+            
+        } else {
+    
+            $('.first').next().removeClass('active');
+            $('.first').addClass('active');
+            $('.first-index').addClass('index-image-color');
+            $('.first-index').next().removeClass('index-image-color');
+            
+        }
+
+    }
+
+    
+    
+    
+}
+
+function keyDirection (keyCode) {
+    
+    if (keyCode == 39) {
+            
+        if (( $('.first').hasClass('active') ) && ( $('.first-index').hasClass('index-image-color') ) ) {
+        
+            $('.first').removeClass('active').addClass('display_none');
+            $('.first').next().addClass('active');
+            $('.first-index').removeClass('index-image-color');
+            $('.first-index').next().addClass('index-image-color');
+            
+        
         } else if ( $('.first').next().hasClass('active') ) {
             
             $('.first').next().removeClass('active');           
@@ -38,12 +138,10 @@ jQuery(document).ready(function ($) {
         
         }
 
-    });
-
-    arrowLeft.click( function() {
+    } else if ( keyCode == 37 ) {
         
         if ( $('.first').hasClass('active') ) {
-            
+        
             $('.first').removeClass('active').addClass('display_none');
             $('.last').addClass('active');
             $('.first-index').removeClass('index-image-color');
@@ -72,82 +170,6 @@ jQuery(document).ready(function ($) {
             $('.first-index').next().removeClass('index-image-color');
             
         }
-
-    });
-
-
-    
-    $(document).keydown(function (e) { 
-        console.log(e.keyCode);
-
-        if (e.keyCode == 39) {
-            
-            if (( $('.first').hasClass('active') ) && ( $('.first-index').hasClass('index-image-color') ) ) {
-            
-                $('.first').removeClass('active').addClass('display_none');
-                $('.first').next().addClass('active');
-                $('.first-index').removeClass('index-image-color');
-                $('.first-index').next().addClass('index-image-color');
-                
-             
-            } else if ( $('.first').next().hasClass('active') ) {
-                
-                $('.first').next().removeClass('active');           
-                $('.last').prev().addClass('active');   
-                $('.first-index').next().removeClass('index-image-color');
-                $('.last-index').prev().addClass('index-image-color');
-    
-            } else if ( $('.last').prev().hasClass('active') ) {
-    
-                $('.last').prev().removeClass('active');
-                $('.last').addClass('active');
-                $('.last-index').prev().removeClass('index-image-color');
-                $('.last-index').addClass('index-image-color');
-                
-            } else {
-    
-                $('.last').removeClass('active');
-                $('.first').addClass('active');
-                $('.last-index').removeClass('index-image-color');
-                $('.first-index').addClass('index-image-color');
-            
-            }
-
-        } else if ( e.keyCode == 37 ) {
-            
-            if ( $('.first').hasClass('active') ) {
-            
-                $('.first').removeClass('active').addClass('display_none');
-                $('.last').addClass('active');
-                $('.first-index').removeClass('index-image-color');
-                $('.last-index').addClass('index-image-color');
-    
-            
-            } else if ( $('.last').hasClass('active') ) {
-                
-                $('.last').removeClass('active');           
-                $('.last').prev().addClass('active');
-                $('.last-index').prev().addClass('index-image-color');
-                $('.last-index').removeClass('index-image-color');
-    
-            } else if ( $('.last').prev().hasClass('active')) {
-    
-                $('.last').prev().removeClass('active');
-                $('.first').next().addClass('active');
-                $('.first-index').next().addClass('index-image-color');
-                $('.last-index').prev().removeClass('index-image-color');
-                
-            } else {
-    
-                $('.first').next().removeClass('active');
-                $('.first').addClass('active');
-                $('.first-index').addClass('index-image-color');
-                $('.first-index').next().removeClass('index-image-color');
-                
-            }
-            
-        }
         
-    });
-    
-}); // Fine Documento 
+    }
+}
